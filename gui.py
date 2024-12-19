cb7e811e4148ac85116cfdf2ee415442bd97a707
jobi joseph <jobijoseph16@gmail.com>
2024-12-19 16:54:33 +0530

    created simple GUI

---
+++ gui.py
@@ -0,0 +1,10 @@
+import function
+import FreeSimpleGUI as si
+
+label = si.Text("Type a to-do")
+input_box = si.InputText(tooltip="Enter todo")
+add_button = si.Button("Add")
+
+window = si.Window('My To-Do App', layout=[[label], [input_box,add_button ]])
+window.read()
+window.close()
