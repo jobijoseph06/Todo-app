71cf763254726c672f97de3659f25d0655db14b1
jobi joseph <jobijoseph16@gmail.com>
2024-12-20 22:40:09 +0530

    implemented the edit button

--- project/gui.py
+++ project/gui.py
@@ -5,20 +5,33 @@ import function
 label = si.Text("Type a to-do")
 input_box = si.InputText(tooltip="Enter todo", key='todo')
 add_button = si.Button("Add")
+list_box = si.Listbox(values=function.get_todo() , key='todos', enable_events=True, size=[45, 10])
+edit_button = si.Button("Edit")
 
-
-window = si.Window('My To-Do App', layout=[[label], [input_box,add_button ]], font=('Helvetica', 20))
+window = si.Window('My To-Do App', layout=[[label], [input_box,add_button ],[list_box, edit_button] ], font=('Helvetica', 17))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values['todos'])
     match event:
         case "Add":
             todos = function.get_todo()
             new_todo1 = values['todo'] + "\n"
             todos.append(new_todo1)
             function.write_todo(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = function.get_todo()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            function.write_todo(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case si.WINDOW_CLOSED:
             break
 
